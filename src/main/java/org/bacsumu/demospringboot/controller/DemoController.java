9e9ba90df5a26028082d66118b5ab853f35babb8
bacsumu <bacsumu@gmail.com>
2023-09-08 16:14:09 +0900

    test

--- src/main/java/org/bacsumu/demospringboot/controller/DemoController.java
+++ src/main/java/org/bacsumu/demospringboot/controller/DemoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
-    @RequestMapping(value = "/home")
+    @RequestMapping(value = "/")
     public String home(){
         return "hello home";
     }
