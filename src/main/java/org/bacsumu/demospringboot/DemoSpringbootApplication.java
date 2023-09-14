44c932a484fcd9ef4ea2ad24fed3a66be054555e
조용필-BACSUMU\Administrator <bacsumu@medialog.co.kr>
2023-09-14 17:10:47 +0900

    test

--- src/main/java/org/bacsumu/demospringboot/DemoSpringbootApplication.java
+++ src/main/java/org/bacsumu/demospringboot/DemoSpringbootApplication.java
@@ -2,12 +2,19 @@ package org.bacsumu.demospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class DemoSpringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoSpringbootApplication.class, args);
+		try {
+			SpringApplication.run(DemoSpringbootApplication.class, args);
+		} catch (Exception e) {
+			log.error("error={}",e);
+		}
+		
 	}
 
 }
--- src/main/java/org/bacsumu/demospringboot/controller/DemoController.java
+++ src/main/java/org/bacsumu/demospringboot/controller/DemoController.java
@@ -2,11 +2,14 @@ package org.bacsumu.demospringboot.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
+@Slf4j
 public class DemoController {
     @RequestMapping(value = "/")
     public String home(){
+        log.info("home is called");
         return "this is github springboot demo page";
     }
 }
